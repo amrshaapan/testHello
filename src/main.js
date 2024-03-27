204b8265f8d52c5b9676ed5f31862e2c20fcd931
amrshapaan <amrshaapan@gmail.com>
2024-03-27 02:40:59 +0200

    firstrun servehtt777777

--- src/main.js
+++ src/main.js
@@ -18,13 +18,13 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.VUE_APP_WEBSOCKETS_KEY,
     wsHost: process.env.VUE_APP_WEBSOCKETS_SERVER,
-    wssHost: process.env.VUE_APP_WEBSOCKETS_SERVER,
+    // wssHost: process.env.VUE_APP_WEBSOCKETS_SERVER,
     wsPort: 6001,
     cluster:'mt1',
     forceTLS: false,
     disableStats:true,
 
-    wssPort: 6001,
+    // wssPort: 6001,
 
     enabledTransports: ['ws'],
 
